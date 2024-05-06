6a0678bce013e7996b06572ca79268598e78dc6b
Sidryll <sidryl.gerardo-21@cpu.edu.ph>
2024-05-06 23:08:41 +0800

    DashBoard Fix - 1

--- server/index.js
+++ server/index.js
@@ -1,79 +1,121 @@
-    //dependencies
-    const express = require('express')
-    const app = express()
-    const mysql = require('mysql')
-    const cors = require('cors')
+//dependencies
+const express = require("express");
+const app = express();
+const mysql = require("mysql");
+const cors = require("cors");
 
-    app.use(express.json())
-    app.use(cors())
+app.use(express.json());
+app.use(cors());
 
-    app.listen(3002, ()=>{
-        console.log('Server is running on port 3002')
-    })
+app.listen(3002, () => {
+  console.log("Server is running on port 3002");
+});
 
-    //mysql
+//mysql
 
-    const db = mysql.createConnection({
-        user: 'root',
-        host: 'localhost',
-        password: '',
-        database: 'userdb',
-    })
+const db = mysql.createConnection({
+  user: "root",
+  host: "localhost",
+  password: "",
+  database: "users",
+});
 
-    app.get('/check-email', (req, res) => {
-        const { email } = req.query;
-        const SQL = 'SELECT email FROM users WHERE email = ?';
-        db.query(SQL, [email], (err, results) => {
-            if (err) {
-                res.status(500).send({ error: err.message });
-            } else {
-                res.send({ exists: results.length > 0 });
-            }
-        });
-    });
+app.get("/check-email", (req, res) => {
+  const { email } = req.query;
+  const SQL = "SELECT email FROM users WHERE email = ?";
+  db.query(SQL, [email], (err, results) => {
+    if (err) {
+      res.status(500).send({ error: err.message });
+    } else {
+      res.send({ exists: results.length > 0 });
+    }
+  });
+});
 
-    app.post('/register', (req, res)=>{
-        const sentEmail = req.body.Email
-        const sentPassword = req.body.Password
-        const sentBirthdate = req.body.Birthdate
-        const sentFirstName = req.body.FirstName
-        const sentMiddleName = req.body.MiddleName
-        const sentLastName = req.body.LastName
-        const sentMobileNumber = req.body.MobileNumber
+app.post("/register", (req, res) => {
+  const sentEmail = req.body.Email;
+  const sentPassword = req.body.Password;
+  const sentBirthdate = req.body.Birthdate;
+  const sentFirstName = req.body.FirstName;
+  const sentMiddleName = req.body.MiddleName;
+  const sentLastName = req.body.LastName;
+  const sentMobileNumber = req.body.MobileNumber;
 
-        const SQL = 'INSERT INTO users (email, password, birthdate, firstname, middlename, lastname, mobilenumber) VALUES (?,?,?,?,?,?,?)'
+  const SQL =
+    "INSERT INTO users (email, password, birthdate, firstname, middlename, lastname, mobilenumber) VALUES (?,?,?,?,?,?,?)";
 
-        const Values = [sentEmail, sentPassword, sentBirthdate, sentFirstName, sentMiddleName, sentLastName, sentMobileNumber]
+  const Values = [
+    sentEmail,
+    sentPassword,
+    sentBirthdate,
+    sentFirstName,
+    sentMiddleName,
+    sentLastName,
+    sentMobileNumber,
+  ];
 
-        db.query(SQL, Values, (err, results)=>{
-            if(err){
-                res.send(err)
-            }
-            else{
-                console.log('User Inserted Succesfully!')
-                res.send({message: 'User Added!'})
-            }   
-        })
-        
-    })
+  db.query(SQL, Values, (err, results) => {
+    if (err) {
+      res.send(err);
+    } else {
+      console.log("User Inserted Succesfully!");
+      res.send({ message: "User Added!" });
+    }
+  });
+});
 
-    app.post('/login', (req, res)=>{
-        const sentLoginEmail = req.body.LoginEmail
-        const sentLoginPassword = req.body.LoginPassword
+app.post("/login", (req, res) => {
+  const sentLoginEmail = req.body.LoginEmail;
+  const sentLoginPassword = req.body.LoginPassword;
 
-        const SQL = 'SELECT * FROM users WHERE email = ? && password = ?'
+  const SQL = "SELECT * FROM users WHERE email = ? && password = ?";
 
-        const Values = [sentLoginEmail, sentLoginPassword]
+  const Values = [sentLoginEmail, sentLoginPassword];
 
-        db.query(SQL, Values, (err, results)=>{
-            if(err){
-                res.send({error: err})
-            }
-            if (results.length > 0){
-                res.send(results)
-            }
-            else{
-                res.send({message: 'Credentials does not match!'})
-            }
-        })
-    })
+  db.query(SQL, Values, (err, results) => {
+    if (err) {
+      res.send({ error: err });
+    }
+    if (results.length > 0) {
+      res.send(results[0]);
+    } else {
+      res.send({ message: "Credentials does not match!" });
+    }
+  });
+});
+
+app.get("/getUser/:id", (req, res) => {
+  const userId = parseInt(req.params.id);
+  const SQL = "SELECT * FROM users WHERE id = ?";
+  const values = [userId];
+
+  db.query(SQL, values, (err, results) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+    } else {
+      if (results.length > 0) {
+        res.json(results[0]); // Assuming there should be only one user with a given ID
+      } else {
+        res.status(404).json({ message: "No user found" });
+      }
+    }
+  });
+});
+
+app.patch("/update/:id", (req, res) => {
+  const userId = parseInt(req.params.id);
+  const { firstname, middlename, lastname, suffix, address } = req.body;
+  const SQL =
+    "UPDATE users SET firstname = ? , middlename = ?, lastname = ?, suffix = ?, address = ? WHERE id = ?";
+  const values = [firstname, middlename, lastname, suffix, address, userId];
+
+  db.query(SQL, values, (err, results) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+    } else {
+      db.query(`SELECT * FROM users WHERE id = ${userId}`, (err, results) => {
+        res.status(200).json(results);
+      });
+    }
+  });
+});
